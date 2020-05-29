a2848e19c406365e22044c38802b297162284b01
kim-uhyeon <kim-uhyeon@localhost>
2020-05-29 19:36:20 +0900

    null

--- PizzaProject/src/pizza/AllViewMain.java
+++ PizzaProject/src/pizza/AllViewMain.java
@@ -3,19 +3,18 @@ package pizza;
 public class AllViewMain {
 	public static void main(String[] args) {
 
-//		new PizzaPickView();
-//		System.out.println("실행");
+		new FirstUI();
+		
+		
+		
+		 /************TEAM************
+		 * 박세준 :: 프로젝트 총괄
+		 * 박준일 :: DB담당
+		 * 안은지 :: UI 담당
+		 * 배정언 :: UI 담당
+		 * 김우현 :: UI 총괄, Control Support
+		 * Our Project :: https://github.com/pkt369/PizzaProject
+		 *****************************/
 
-		
-//		new FirstUI();
-		
-		new LastUI();
-		
-		
-		
-
-		
-
-		
 	}
 }
